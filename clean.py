0c84d19930061fb54a1f0a631030e044578e20a1
andrewrowleyhippo <andrew.rowley@hippodigital.co.uk>
2025-12-15 16:30:23 +0000

    cleaning script done

--- clean.py
+++ clean.py
@@ -1,11 +1,36 @@
 '''Take a csv of weather data and parse it.'''
 
-def load_data():
-    pass
 
-def clean_data():
-    pass
+import pandas as pd
+
+def snakecasify_columns(df):
+    '''For a pandas dataframe, coerce column names to snake_case.'''
+    cols = df.columns.tolist()
+    cols_mod = [0]*len(cols)
+    for i, c in enumerate(cols):
+        cols_mod[i] = c.lower().strip().replace(" ","_")
+    d = dict(zip(cols, cols_mod))
+    df.rename(columns=d, inplace=True)
+    return df
+
+
+def extract_data_to_df(path):
+    df = pd.read_csv(path, header = 0)
+    return df
+
+
+def clean_df(df):
+    df = snakecasify_columns(df)
+    df.drop([
+        'date.month',
+        'date.week_of',
+        'date.year',
+        'station.city'
+        ], axis=1)
+    return df
+
 
 if __name__ == "__main__":
-    load_data()
-    clean_data()
+    path = 'data/weather.csv'
+    df = extract_data_to_df(path)
+    clean_df(df)
