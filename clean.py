6e4c13094ffbbf640858a0125150f512d0412b8b
andrewrowleyhippo <andrew.rowley@hippodigital.co.uk>
2025-12-16 08:54:06 +0000

    finish clean.py

--- clean.py
+++ clean.py
@@ -1,36 +1,31 @@
-'''Take a csv of weather data and parse it.'''
-
+"""Take a csv of weather data and parse it."""
 
 import pandas as pd
 
+
 def snakecasify_columns(df):
-    '''For a pandas dataframe, coerce column names to snake_case.'''
+    """For a pandas dataframe, coerce column names to snake_case."""
     cols = df.columns.tolist()
-    cols_mod = [0]*len(cols)
+    cols_mod = [0] * len(cols)
     for i, c in enumerate(cols):
-        cols_mod[i] = c.lower().strip().replace(" ","_")
+        cols_mod[i] = c.lower().strip().replace(" ", "_")
     d = dict(zip(cols, cols_mod))
     df.rename(columns=d, inplace=True)
     return df
 
 
 def extract_data_to_df(path):
-    df = pd.read_csv(path, header = 0)
+    df = pd.read_csv(path, header=0)
     return df
 
 
 def clean_df(df):
     df = snakecasify_columns(df)
-    df.drop([
-        'date.month',
-        'date.week_of',
-        'date.year',
-        'station.city'
-        ], axis=1)
+    df.drop(["date.month", "date.week_of", "date.year", "station.city"], axis=1)
     return df
 
 
 if __name__ == "__main__":
-    path = 'data/weather.csv'
+    path = "data/weather.csv"
     df = extract_data_to_df(path)
-    clean_df(df)
+    dfx = clean_df(df)
